ac7fa5aab37887fb122300e88f52d014352204e7
刘欣雨 <liuxinyu@atwasoft.net>
2022-01-10 15:37:01 +0800

    登录页修改

--- static/js/login.js
+++ static/js/login.js
@@ -16,3 +16,23 @@ function sendVerification () {
 
   })
 }
+// 提交点击
+function loginSubmit () {
+  if (!$("#phone").val()) {
+    $.alert('请输入手机号')
+    return
+  }
+  if (!$("#yzm").val()) {
+    $.alert('请输入验证码')
+    return
+  }
+  if ($("#yzm").val().length !== 6) {
+    $.alert('验证码有误')
+    return
+  }
+  console.log('11', $("#weuiAgree").is(':checked'))
+  if (!$("#weuiAgree").is(':checked')) {
+    $.alert('请勾选已阅读并同意')
+    return
+  }
+}
