d8d9c5cc4285510d43e36f872f8d0c2515c9fd67
刘欣雨 <liuxinyu@atwasoft.net>
2022-01-11 13:11:41 +0800

    视频封面

--- static/js/pet_detail.js
+++ static/js/pet_detail.js
@@ -3,7 +3,7 @@ $(function () {
   console.log("id==", id)
   getPetDetail(id)
 })
-function getPetDetail(id) {
+function getPetDetail (id) {
   let param = {
     pet_id: id,
   }
@@ -17,7 +17,7 @@ function getPetDetail(id) {
     }
   })
 }
-function showPetPge(data) {
+function showPetPge (data) {
   // 顶部图
   $("#topImg").attr("src", data.avatar)
   $("#petNo").html("编号：" + data.pet_no)
@@ -32,37 +32,35 @@ function showPetPge(data) {
   $("#yunyang_price").html("￥" + fmPrice(data.first_price / 100))
   showVideos(data.files)
 }
-function showVideos(files) {
+async function showVideos (files) {
   let html = ""
   const len = files.length
   if (files && len) {
     let item = files[0]
-
+    // const videoImg = await this.getVideoBase64(item.file_url) // video的url
+    // getVideoFrame(3);
     html = `
     
     <div class="video_item">
       <div style="line-height:8vw;">拍摄于${formatTime(item.add_time)}</div>
-      <video src="${
-        item.file_url
-      }" class="pet_video"  webkit-playsinline="true" controls="controls" id="myVideo" ></video>
+      <video src="${item.file_url}" class="pet_video"  webkit-playsinline="true" controls="controls" id="myVideo" poster="/static/images/home/cat.png" ></video>
     </div>      
     `
     html += `
     <div class="video_item">
       <div class="pet_video"  style="filter:contrast(5%) blur(5px);background:black;height:100vw"></div>
-      ${
-        len > 1
-          ? `<p class="unlock_video">云养成功后解锁剩余${len - 1}个视频</p>`
-          : ""
+      ${len > 1
+        ? `<p class="unlock_video">云养成功后解锁剩余${len - 1}个视频</p>`
+        : ""
       } 
     </div>      
     `
     $("#pet_movies").html(html)
-    // setPoster()
-    let poster = ""
+
     getVideoBase64(item.file_url).then((res) => {
       poster = res
-      console.log("111", poster)
+      // console.log("111", poster)
+      $("#myVideo").attr('poster', res)
     })
   } else {
     html = "<div class='no_data'>暂无数据</div>"
@@ -70,50 +68,55 @@ function showVideos(files) {
   }
 }
 
-function setPoster() {
-  var scale = 0.8 //第一帧图片与源视频的比例
-  let video = $("#myVideo") //.get(0) //赋值标签
-  $("#myVideo").on("loadeddata", function (e) {
-    //加载完成事件，调用函数
-    var canvas = document.createElement("canvas") //canvas画布
-    canvas.width = video.videoWidth * scale
-    canvas.height = video.videoHeight * scale
-    canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height) //画图
-    video.setAttribute("poster", canvas.toDataURL("image/png")) //关键一步 —— 设置标签的 poster 属性的值为 base64 编译过后的canvas绘图。
-    console.log("post", canvas.toDataURL("image/png"))
-  })
 
-  // video.on("loadeddata", function () {
-  //   //加载完成事件，调用函数
-  //   var canvas = document.createElement("canvas") //canvas画布
-  //   canvas.width = video.videoWidth * scale
-  //   canvas.height = video.videoHeight * scale
-  //   canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height) //画图
-  //   video.setAttribute("poster", canvas.toDataURL("image/png")) //关键一步 —— 设置标签的 poster 属性的值为 base64 编译过后的canvas绘图。
-  //   console.log("post", canvas.toDataURL("image/png"))
-  // })
-}
-function getVideoBase64(url) {
+
+function getVideoBase64 (url) {
+  // console.log('url', url)
   return new Promise(function (resolve, reject) {
-    let dataURL = ""
-    // let video = document.createElement("video")
-    // video.setAttribute("crossOrigin", "anonymous") //处理跨域
-    // video.setAttribute("src", url)
-    // video.setAttribute("width", 400)
-    // video.setAttribute("height", 240)
-    // video.setAttribute("preload", "auto")
-    let video = document.getElementById("myVideo")
-    console.log("video", video)
-    video.addEventListener("loadeddata", function () {
-      let canvas = document.createElement("canvas"),
-        width = video.width, //canvas的尺寸和图片一样
-        height = video.height
+    let dataURL = ''
+    const video = document.createElement('video')
+    video.setAttribute('crossOrigin', 'Anonymous') // 处理跨域
+    video.setAttribute('src', url)
+    // 可以设置创建视频有固定的宽高
+    // video.setAttribute('width', 400)
+    // video.setAttribute('height', 240)
+    video.setAttribute('preload', 'auto') // auto|metadata|none
+    video.addEventListener('loadeddata', function () {
+      const canvas = document.createElement('canvas')
+      // canvas的尺寸和设置的视频宽高一样
+      // const width = video.width
+      // const height = video.height
+      // 如果未设置创建时视频的宽高，则使用默认视频的宽高
+      const width = video.videoWidth
+      const height = video.videoHeight
       canvas.width = width
       canvas.height = height
-      canvas.getContext("2d").drawImage(video, 0, 0, width, height) //绘制canvas
-      dataURL = canvas.toDataURL("image/jpeg") //转换为base64
-      console.log("dataURL", dataURL)
+      canvas.getContext('2d').drawImage(video, 0, 0, width, height) // 绘制canvas
+      dataURL = canvas.toDataURL('image/jpeg') // 转换为base64
+      // console.log('dataURL:', dataURL) // base64格式的地址
       resolve(dataURL)
     })
   })
 }
+/**
+ * 获取视频封面
+ * @param {*} videoUrl 视频链接
+ * @param {*} frames 帧数
+ */
+// const getVideoFrame = (videoUrl, frames) => {
+//   const video = document.createElement('video');
+//   video.width = 200;
+//   video.height = 200;
+//   video.src = videoUrl;
+//   video.currentTime = 1 / 60 * frames;
+//   video.setAttribute('crossOrigin', 'anonymous');
+//   video.oncanplay = () => {
+//     const oCanvas = document.createElement('canvas');
+//     oCanvas.width = video.width;
+//     oCanvas.height = video.width;
+//     oCanvas.getContext("2d").drawImage(video, 0, 0, video.width, oCanvas.height);
+//     const base64 = oCanvas.toDataURL("image/png");
+//     console.log(base64, '图片地址');
+//   }
+// };
+
