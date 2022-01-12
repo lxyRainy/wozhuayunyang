123762895a0a8a28d798a8ba2aa53b90832d3c91
刘欣雨 <liuxinyu@atwasoft.net>
2022-01-12 16:05:14 +0800

    黑图

--- static/js/pet_detail.js
+++ static/js/pet_detail.js
@@ -38,13 +38,15 @@ async function showVideos (files) {
   const len = files.length
   if (files && len) {
     let item = files[0]
+
     // const videoImg = await this.getVideoBase64(item.file_url) // video的url
     // getVideoFrame(3);
     html = `
     <div class="video_item">
       <div style="line-height:8vw;">拍摄于${formatTime(item.add_time)}</div>
-      <video src="${item.file_url
-      }" class="pet_video"  webkit-playsinline="true" controls="controls" id="myVideo" poster="static/images/home/cat.png" ></video>
+      <video  class="pet_video"  webkit-playsinline="true" controls="controls" id="myVideo"  style="object-fit:cover" x5-video-player-type="h5" poster="static/images/pet_detail/black.jpg" >
+      <source src="${item.file_url}">
+      </video>
     </div>      
     `
     html += `
@@ -58,10 +60,17 @@ async function showVideos (files) {
     `
     $("#pet_movies").html(html)
 
-    getVideoBase64(item.file_url).then((res) => {
-      poster = res
-      // console.log("111", poster)
-      $("#myVideo").attr("poster", res)
+    // 这里通过http-server将视频地址与js进行同源
+    const videoSrc = item.file_url
+    // 饱和度品质 0/10/30/50
+    const targetSaturation = 0
+    getBestPoster(videoSrc, targetSaturation).then(posterUrl => {
+      // const image = new Image()
+      // image.src = posterUrl
+      // document.body.append(image)
+      $("#myVideo").attr('poster', posterUrl)
+    }).catch(error => {
+      console.log(error)
     })
   } else {
     html = "<div class='no_data'>暂无数据</div>"
@@ -69,55 +78,163 @@ async function showVideos (files) {
   }
 }
 
-function getVideoBase64 (url) {
-  // console.log('url', url)
-  return new Promise(function (resolve, reject) {
-    let dataURL = ""
-    const video = document.createElement("video")
-    video.setAttribute("crossOrigin", "Anonymous") // 处理跨域
-    video.setAttribute("src", url)
-    // 可以设置创建视频有固定的宽高
-    // video.setAttribute('width', 400)
-    // video.setAttribute('height', 240)
-    video.setAttribute("preload", "auto") // auto|metadata|none
-    video.addEventListener("loadeddata", function () {
-      const canvas = document.createElement("canvas")
-      // canvas的尺寸和设置的视频宽高一样
-      // const width = video.width
-      // const height = video.height
-      // 如果未设置创建时视频的宽高，则使用默认视频的宽高
-      const width = video.videoWidth
-      const height = video.videoHeight
-      canvas.width = width
-      canvas.height = height
-      canvas.getContext("2d").drawImage(video, 0, 0, width, height) // 绘制canvas
-      dataURL = canvas.toDataURL("image/jpeg") // 转换为base64
-      // console.log('dataURL:', dataURL) // base64格式的地址
-      resolve(dataURL)
+// 获取视频基本信息
+function getVideoBasicInfo (videoSrc) {
+  return new Promise((resolve, reject) => {
+    const video = document.createElement('video')
+    video.src = videoSrc
+    // 视频一定要添加预加载
+    video.preload = 'auto'
+    // 视频一定要同源或者必须允许跨域
+    video.crossOrigin = 'Anonymous'
+    // 监听：异常
+    video.addEventListener('error', error => {
+      reject(error)
+    })
+    // 监听：加载完成基本信息,设置要播放的时常
+    video.addEventListener('loadedmetadata', () => {
+      const videoInfo = {
+        video,
+        width: video.videoWidth,
+        height: video.videoHeight,
+        duration: video.duration
+      }
+      resolve(videoInfo)
     })
   })
 }
-/**
- * 获取视频封面
- * @param {*} videoUrl 视频链接
- * @param {*} frames 帧数
- */
-// const getVideoFrame = (videoUrl, frames) => {
-//   const video = document.createElement('video');
-//   video.width = 200;
-//   video.height = 200;
-//   video.src = videoUrl;
-//   video.currentTime = 1 / 60 * frames;
-//   video.setAttribute('crossOrigin', 'anonymous');
-//   video.oncanplay = () => {
-//     const oCanvas = document.createElement('canvas');
-//     oCanvas.width = video.width;
-//     oCanvas.height = video.width;
-//     oCanvas.getContext("2d").drawImage(video, 0, 0, video.width, oCanvas.height);
-//     const base64 = oCanvas.toDataURL("image/png");
-//     console.log(base64, '图片地址');
-//   }
-// };
+// 获取视频基本信息
+function getVideoBasicInfo (videoSrc) {
+  return new Promise((resolve, reject) => {
+    const video = document.createElement('video')
+    video.src = videoSrc
+    // 视频一定要添加预加载
+    video.preload = 'auto'
+    // 视频一定要同源或者必须允许跨域
+    video.crossOrigin = 'Anonymous'
+    // 监听：异常
+    video.addEventListener('error', error => {
+      reject(error)
+    })
+    // 监听：加载完成基本信息,设置要播放的时常
+    video.addEventListener('loadedmetadata', () => {
+      const videoInfo = {
+        video,
+        width: video.videoWidth,
+        height: video.videoHeight,
+        duration: video.duration
+      }
+      resolve(videoInfo)
+    })
+  })
+}
+
+// 将获取到的视频信息，转化为图片地址
+function getVideoPosterInfo (videoInfo) {
+  return new Promise(resolve => {
+    const { video, width, height } = videoInfo
+    video.addEventListener('canplay', () => {
+      const canvas = document.createElement('canvas')
+      canvas.width = width
+      canvas.height = height
+      const ctx = canvas.getContext('2d')
+      ctx.drawImage(video, 0, 0, width, height)
+      const saturation = getImageSaturation(canvas)
+      const posterUrl = canvas.toDataURL('image/jpg')
+      resolve({ posterUrl, saturation })
+    })
+  })
+}
+// 获取一个图片的平均饱和度
+function getImageSaturation (canvas) {
+  const ctx = canvas.getContext('2d')
+  const uint8ClampedArray = ctx.getImageData(0, 0, canvas.width, canvas.height).data
+  console.log(uint8ClampedArray)
+  const rgbaList = binary2rgba(uint8ClampedArray)
+  const hslList = rgbaList.map(item => {
+    return rgb2hsl(item.r, item.g, item.b)
+  })
+  const avarageSaturation = hslList.reduce((total, curr) => total + curr.s, 0) / hslList.length
+  return avarageSaturation
+}
+
+function rgb2hsl (r, g, b) {
+  r = r / 255;
+  g = g / 255;
+  b = b / 255;
+
+  var min = Math.min(r, g, b);
+  var max = Math.max(r, g, b);
+  var l = (min + max) / 2;
+  var difference = max - min;
+  var h, s, l;
+  if (max == min) {
+    h = 0;
+    s = 0;
+  } else {
+    s = l > 0.5 ? difference / (2.0 - max - min) : difference / (max + min);
+    switch (max) {
+      case r: h = (g - b) / difference + (g < b ? 6 : 0); break;
+      case g: h = 2.0 + (b - r) / difference; break;
+      case b: h = 4.0 + (r - g) / difference; break;
+    }
+    h = Math.round(h * 60);
+  }
+  s = Math.round(s * 100);//转换成百分比的形式
+  l = Math.round(l * 100);
+  return { h, s, l };
+}
+
+function binary2rgba (uint8ClampedArray) {
+  const rgbaList = []
+  for (let i = 0; i < uint8ClampedArray.length; i++) {
+    if (i % 4 === 0) {
+      rgbaList.push({ r: uint8ClampedArray[i] })
+      continue
+    }
+    const currentRgba = rgbaList[rgbaList.length - 1]
+    if (i % 4 === 1) {
+      currentRgba.g = uint8ClampedArray[i]
+      continue
+    }
+    if (i % 4 === 2) {
+      currentRgba.b = uint8ClampedArray[i]
+      continue
+    }
+    if (i % 4 === 3) {
+      currentRgba.a = uint8ClampedArray[i]
+      continue
+    }
+  }
+  return rgbaList
+}
+
+// 根据视频地址与播放时长获取图片信息与图片平均饱和度
+function getVideoPosterByFrame (videoSrc, targetTime) {
+  return getVideoBasicInfo(videoSrc).then(videoInfo => {
+    const { video, duration } = videoInfo
+    video.currentTime = targetTime
+    return getVideoPosterInfo(videoInfo)
+  })
+}
+
+
+
+async function getBestPoster (videoSrc, targetSaturation) {
+  const videoInfo = await getVideoBasicInfo(videoSrc)
+  const { duration } = videoInfo
+  for (let i = 0; i <= duration; i++) {
+    const posterInfo = await getVideoPosterByFrame(videoSrc, i)
+    const { posterUrl, saturation } = posterInfo
+    // 判断当前饱和度是否大于等于期望的饱和度
+    if (saturation >= targetSaturation) {
+      return posterUrl
+    }
+  }
+}
+
+
+
 
 function yunyangClick (params) {
   if (!sfLogin) {
